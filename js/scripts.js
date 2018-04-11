f2581f9003d6d59a6667ea8e014ba9e8cd6772ac
Daniel Springer <dssp100@gmail.com>
2018-04-11 19:15:40 -0400

    Completed Section 2.11: JavaScript & APIs

--- js/scripts.js
+++ js/scripts.js
@@ -17,22 +17,6 @@
     }(document, 'script', 'facebook-jssdk'));
 
 //Google maps API
-
-//Simple Map
-
-// function initMap() {
-//   var uluru = {lat: 43.7340, lng: 7.4210};
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//     zoom: 15,
-//     center: uluru
-//   });
-//   var marker = new google.maps.Marker({
-//     position: uluru,
-//     map: map
-//   });
-// }
-
-
 function initMap() {
         var myLatlng = {lat: 43.7340, lng: 7.4210};
 
